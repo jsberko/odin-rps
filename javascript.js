13e45a58baa7b4df2bd000b4aabdabb51885e07c
jsberko <147778552+jsberko@users.noreply.github.com>
2024-11-08 13:33:35 -0500

    Fix bugs and clean up code

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
-// Game Object
+// Objects
 const game = {
     // Variables
     roundWinner: "",
+    isOver: false,
 
     // Selectors
     image: document.querySelector("#image"),
+
     playGameButtons: document.querySelector("#playGameButtonsContainer"),
     changeGameButtons: document.querySelector("#changeGameButtonsContainer"),
+
     messageDisplay: document.querySelector("#messageDisplay"),
     playerScoreDisplay: document.querySelector("#playerScore"),
     computerScoreDisplay: document.querySelector("#computerScore"),
@@ -15,80 +18,49 @@ const game = {
 
     // Messages
     newGameMsg: "It's you vs. computer, first one to 5 points wins! Make your selection below to begin play.",
-    humanWinsMsg: "Game over. You have defeated the computer! Odin thanks you for your capable efforts.",
-    computerWinsMsg: "Game over. You have been defeated by the computer! Odin scowls in disgrace.",
+    humanWinsMsg: "Game over. You have defeated the computer! Odin cheers you for your capable efforts.",
+    computerWinsMsg: "Game over. You have been defeated by the computer! Odin scowls at your disgrace.",
 }
 
-// Video Object
+
 const video = {
     container: document.querySelector("#videoContainer"),
     source: document.querySelector("#videoSource"),
 }
 
 
-// Player Object
 const player = {
     score: 0,
     winTotal: 0,
 }
 
 
-// Computer Object
 const computer = {
     score: 0,
     winTotal: 0,
 }
 
 
+
+
 // Functions
-function playVideo(roundResult) {
-    game.image.classList.add("hide");
-    video.container.classList.remove("hide");
-    video.source.setAttribute("src", roundResult);
-    video.container.load();
-    video.container.play();
-}
-
-function chooseVideo(playerChoice, computerChoice) {
-    const roundResult = `${playerChoice}${computerChoice}`;
-
-    switch (roundResult) {
-        case "RockRock": return "./video/RockRock.mp4"; break;
-        case "RockPaper": return "./video/RockPaper.mp4"; break;
-        case "RockScissors": return "./video/RockScissors.mp4"; break;
-        case "PaperRock": return "./video/PaperRock.mp4"; break;
-        case "PaperPaper": return "./video/PaperPaper.mp4"; break;
-        case "PaperScissors": return "./video/PaperScissors.mp4"; break;
-        case "ScissorsRock": return "./video/ScissorsRock.mp4"; break;
-        case "ScissorsPaper": return "./video/ScissorsPaper.mp4"; break;
-        case "ScissorsScissors": return "./video/ScissorsScissors.mp4"; break;
-    }
-}
-
-
 function newGame() {
     // Reset variables
+    game.isOver = false;
     player.score = 0;
     computer.score = 0;
     game.roundWinner = "";
 
-    // Reset displays
+    // Reset display
     game.playerScoreDisplay.textContent = "0";
     game.computerScoreDisplay.textContent = "0";
     game.image.setAttribute("src", "./images/rps.jpg");
 
     // Reset message
-    game.messageDisplay.setAttribute("color", "rgb(203, 162, 135)");
+    game.messageDisplay.classList.add("turns-default-color");
+    game.messageDisplay.classList.remove("turns-red");
+    game.messageDisplay.classList.remove("turns-green");
     updateMessage(game.newGameMsg);
-}
-
-function endRound() {
-    updateScores();
-    updateMessage(roundWinner);
-
-    if (player.score === 5 || computer.score === 5) {
-        endGame();
-    }
 }
 
 
@@ -112,25 +84,60 @@ function getComputerChoice() {
     }
     else if (random === 2) {
         return "Paper";
-    } else {
+    }
+    else {
         return "Scissors";
     }
 };
 
 
 function determineWinner(playerChoice, computerChoice) {
-    if (playerChoice === "Rock" && computerChoice === "Rock") {
+    if (playerChoice === computerChoice) {
         return "Tie!";
-        game.image.setAttribute("src", "./video/Rock_Rock.mp4");
     }
-    else if (playerChoice === computerChoice) {
-        return "Tie!";
-    } else if ((playerChoice === "Rock" && computerChoice === "Scissors") || (playerChoice === "Paper" && computerChoice === "Rock") || (playerChoice === "Scissors" && computerChoice === "Paper")) {
+    else if ((playerChoice === "Rock" && computerChoice === "Scissors") || (playerChoice === "Paper" && computerChoice === "Rock") || (playerChoice === "Scissors" && computerChoice === "Paper")) {
         player.score += 1;
         return "You win!"
-    } else {
+    }
+    else {
         computer.score += 1;
         return "Computer wins!";
+    }
+}
+
+
+function chooseVideo(playerChoice, computerChoice) {
+    const roundResult = `${playerChoice}${computerChoice}`;
+
+    switch (roundResult) {
+        case "RockRock": return "./video/RockRock.mp4"; break;
+        case "RockPaper": return "./video/RockPaper.mp4"; break;
+        case "RockScissors": return "./video/RockScissors.mp4"; break;
+        case "PaperRock": return "./video/PaperRock.mp4"; break;
+        case "PaperPaper": return "./video/PaperPaper.mp4"; break;
+        case "PaperScissors": return "./video/PaperScissors.mp4"; break;
+        case "ScissorsRock": return "./video/ScissorsRock.mp4"; break;
+        case "ScissorsPaper": return "./video/ScissorsPaper.mp4"; break;
+        case "ScissorsScissors": return "./video/ScissorsScissors.mp4"; break;
+    }
+}
+
+
+function playVideo(roundResult) {
+    game.image.classList.add("hide");
+    video.container.classList.remove("hide");
+    video.source.setAttribute("src", roundResult);
+    video.container.load();
+    video.container.play();
+}
+
+
+function endRound() {
+    updateScores();
+    updateMessage(roundWinner);
+
+    if (player.score === 5 || computer.score === 5) {
+        setTimeout(endGame, 1000);
     }
 }
 
@@ -146,30 +153,34 @@ function updateMessage(update) {
 }
 
 
-function updateWinTotal() {
-    game.playerWinsDisplay.textContent = player.winTotal;
-    game.computerWinsDisplay.textContent = computer.winTotal;
-}
-
-
 function endGame() {
+    game.isOver = true;
+
     game.image.classList.remove("hide");
     video.container.classList.add("hide");
 
     if (player.score > computer.score) {
         player.winTotal += 1;
         game.image.setAttribute("src", "./images/happy_odin.jpg");
-        game.messageDisplay.setAttribute("color", "green");
+        game.messageDisplay.classList.remove("turns-default-color");
+        game.messageDisplay.classList.add("turns-green")
         updateMessage(game.humanWinsMsg);
         updateWinTotal()
     }
     else {
         computer.winTotal += 1;
         game.image.setAttribute("src", "./images/sad_odin.jpg");
-        game.messageDisplay.setAttribute("color", "red");
+        game.messageDisplay.classList.remove("turns-default-color");
+        game.messageDisplay.classList.add("turns-red")
         updateMessage(game.computerWinsMsg);
         updateWinTotal();
     }
+}
+
+
+function updateWinTotal() {
+    game.playerWinsDisplay.textContent = player.winTotal;
+    game.computerWinsDisplay.textContent = computer.winTotal;
 }
 
 
@@ -182,14 +193,17 @@ function resetGame() {
 
 
 
+
 // Button Event Listener, using Event Delegation
 game.playGameButtons.addEventListener("click", (event) => {
-    let target = event.target;
+    if (!game.isOver) {
+        let target = event.target;
 
-    switch (target.id) {
-        case "rock": playRound("Rock"); updateMessage("Rock...Paper...Scissors SHOOT!"); break;
-        case "paper": playRound("Paper"); updateMessage("Rock...Paper...Scissors SHOOT!"); break;
-        case "scissors": playRound("Scissors"); updateMessage("Rock...Paper...Scissors SHOOT!"); break;
+        switch (target.id) {
+            case "rock": playRound("Rock"); updateMessage("Rock...Paper...Scissors SHOOT!"); break;
+            case "paper": playRound("Paper"); updateMessage("Rock...Paper...Scissors SHOOT!"); break;
+            case "scissors": playRound("Scissors"); updateMessage("Rock...Paper...Scissors SHOOT!"); break;
+        }
     }
 });
 
@@ -197,7 +211,7 @@ game.changeGameButtons.addEventListener("click", (event) => {
     let target = event.target;
 
     switch (target.id) {
-        case "newGame": newGame(); break;
+        case "newGame": if (game.isOver) { newGame() }; break;
         case "resetGame": if (confirm("Reset game?")) { resetGame(); }; break;
     }
 });
